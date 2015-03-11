f469d5e9359274547516e3fa0d35de90793a599d
Justin Noel <justin.noel@calendee.com>
2015-03-11 09:37:46 -0500

    chore : initial commit
    Signed-off-by: Justin Noel <justin.noel@calendee.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # cordovaCameraIssues
 Sample app demonstrating issues with Cordova Camera plugin
+
+## Instructions
+
+```sh
+ionic start cameraIssue https://github.com/calendee/cordovaCameraIssues
+cd cameraIssue
+bower install ngCordova
+cordova plugin add org.apache.cordova.camera
+```
---
+++ js/app.js
@@ -0,0 +1,46 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.controller("CameraController", function($scope, $cordovaCamera) {
+
+  console.log("Camera Controller!");
+  
+  $scope.data = {
+    image : null
+  }
+
+  $scope.takePic = function() {
+
+    console.log("Taking pic!");
+    var options = {
+      destinationType: Camera.DestinationType.FILE_URI,
+      sourceType: Camera.PictureSourceType.CAMERA,
+    };
+
+    $cordovaCamera.getPicture(options).then(function(imageURI) {
+      $scope.data.image = imageURI;
+    }, function(err) {
+      console.log("Failed to capture image!")
+      // error
+    });
+
+  }
+
+})
